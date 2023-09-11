2d7364729ed7606c3d961c98f3f833130ff3e0f0
MFMcNolt <141301431+MFMcNolt@users.noreply.github.com>
2023-09-11 16:07:58 +1000

    created questions, amended HTML JS

---
+++ assets/script.js
@@ -0,0 +1,58 @@
+
+/*timer
+var timeLeft = 60;
+var elem = document.getElementById('Timer');
+
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+    doSomething();
+  }
+}
+*/
+
+/*Quiz function
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		// code will go here
+	}
+
+	function showResults(questions, quizContainer, resultsContainer){
+		// code will go here
+	}
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
+*/
+
+Questions
+var Questions = [{
+    Q: "Which of the following is not a JavaScript Data type?";
+    A: [{Text: "Number", isCorrect: false}
+    {Text: "String", isCorrect: false}
+    {Text: "Bootleg", isCorrect: true}
+    {Text: "Object", isCorrect: false}
+    {Text: "Undefined", isCorrect: false}
+]
+}
+Question: Which of the following is not a JavaScript Data type? Answers: Number, String, Bootleg, Object, Undefined
+
+Question: Arrays in JavaScript can be used to store... Answers: Seeds, Noise, Numbers, Links
+
+Question: Inside what HTML tag you would put JavaScript code? Answers: js, scripting, script, javascript
+
+Question: How do you declare a JavaScript vairable x? Answers: define x, variable x, var x, def x.
+
+Question: when you dont assign a value to a variable it will be? Answers: null, undefined, "", NaN.
+
+
+]

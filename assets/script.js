a654ab616ff87950063a29ff45f25e4ed0f17d17
MFMcNolt <141301431+MFMcNolt@users.noreply.github.com>
2023-09-12 12:01:34 +1000

    amended js

--- assets/script.js
+++ assets/script.js
@@ -42,17 +42,26 @@ var Questions = [{
     {Text: "Bootleg", isCorrect: true}
     {Text: "Object", isCorrect: false}
     {Text: "Undefined", isCorrect: false}
-]
-}
-Question: Which of the following is not a JavaScript Data type? Answers: Number, String, Bootleg, Object, Undefined
-
-Question: Arrays in JavaScript can be used to store... Answers: Seeds, Noise, Numbers, Links
-
-Question: Inside what HTML tag you would put JavaScript code? Answers: js, scripting, script, javascript
-
-Question: How do you declare a JavaScript vairable x? Answers: define x, variable x, var x, def x.
-
-Question: when you dont assign a value to a variable it will be? Answers: null, undefined, "", NaN.
-
-
+    ]}
+    {
+    Q: "Arrays in JavaScript can be used to store...?";
+    A: [{Text: "Seeds", isCorrect: false}
+    {Text: "Noise", isCorrect: false}
+    {Text: "Numbers", isCorrect: true}
+    {Text: "Links", isCorrect: false}
+    ]}
+    {
+    Q: "Inside what HTML tag would you put JavaScript code?";
+    A: [{Text: "js", isCorrect: false}
+    {Text: "scripting", isCorrect: false}
+    {Text: "script", isCorrect: true}
+    {Text: "javascript", isCorrect: false}
+    ]}
+    {
+    Q: "When you dont assign a value to a variable it will be?";
+    A: [{Text: "null", isCorrect: false}
+    {Text: "undefined", isCorrect: true}
+    {Text: "'()'", isCorrect: false}
+    {Text: "NaN", isCorrect: false}
+    ]}
 ]
